a6094b421b2bdaa0e27804c3afb06d7e42475fb9
Teemamin <teemamin@yahoo.com>
2021-08-12 09:20:48 +0400

    edited default Readme

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,19 @@
+import React,{Component,createContext} from "react";
+
+export const ThemeContext = createContext();
+
+class ThemeProvider extends Component{
+    state = {
+        isDarkMode: true
+    }
+
+    render(){
+        return(
+        <ThemeContext.Provider value={{...this.state, someStuff:true}}>
+            {this.props.children}
+        </ThemeContext.Provider>
+        )
+    }
+}
+
+export default ThemeProvider
