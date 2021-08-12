4a096998ee8ed22fd3bdad8605fde6d489b25c29
Teemamin <teemamin@yahoo.com>
2021-08-12 09:35:07 +0400

    added Theme switch functionality, dark to light and vice versa

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -7,9 +7,13 @@ class ThemeProvider extends Component{
         isDarkMode: true
     }
 
+    toggleTheme = ()=>{
+        this.setState({isDarkMode:!this.state.isDarkMode})
+    }
+
     render(){
         return(
-        <ThemeContext.Provider value={{...this.state, someStuff:true}}>
+        <ThemeContext.Provider value={{...this.state, toggleTheme:this.toggleTheme}}>
             {this.props.children}
         </ThemeContext.Provider>
         )
